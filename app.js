d7103b13ecec1b9c7656b0df642b97271705e21e
Fortozo <s.fortozo@accenture.com>
2018-12-14 22:44:22 -0600

    added Procfile and set up ports for heroku

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ var bodyparser = require("body-parser");
 var ejs = require("ejs");
 var mongoose = require('mongoose');
 
-var port = 8080;
+console.log(process.env.PORT);
+app.set('port', process.env.PORT || 8080);
 
 server.listen(port);
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
 	extended: false
 }));
-
-console.log("server online at port "+ port);
 
 app.set("views", __dirname +"/views");
 app.use(express.static(__dirname +"/public"));
@@ -29,8 +28,6 @@ app.set('view engine', 'ejs');
 //WebSocketsss
 const server_socket = require('http').createServer();
 const io = require('socket.io')(server_socket);
-
-server_socket.listen(3000);
 
 io.on('connection', socket => {
 	socket.on('prueba_socketss', (var1, var2) => {
